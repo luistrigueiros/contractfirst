e2eb7525f1ccca0ba94ba96e41d086e1abbea7df
luist <luistrigueiros@gmail.com>
2024-02-11 19:39:07 +0000

    Incremental tidy up

--- src/main/java/com/example/contractfirst/CoursesControllerApiImpl.java
+++ src/main/java/com/example/contractfirst/CoursesControllerApiImpl.java
@@ -1,8 +1,10 @@
 package com.example.contractfirst;
 
 import com.example.openapi.CoursesApi;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "${app.apiRoot}")
 public class CoursesControllerApiImpl implements CoursesApi {
 }
