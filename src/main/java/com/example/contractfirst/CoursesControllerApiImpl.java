bc3928739f923127464d613c3e8e42c790328174
luist <luistrigueiros@gmail.com>
2024-02-11 19:25:07 +0000

    Reverted

--- src/main/java/com/example/contractfirst/CoursesControllerApiImpl.java
+++ src/main/java/com/example/contractfirst/CoursesControllerApiImpl.java
@@ -1,16 +1,8 @@
 package com.example.contractfirst;
 
 import com.example.openapi.CoursesApi;
-import com.example.openapi.models.Course;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class CoursesControllerApiImpl implements CoursesApi {
-    @Override
-    public ResponseEntity<List<Course>> getCourses(String sortBy) {
-        return CoursesApi.super.getCourses(sortBy);
-    }
 }
