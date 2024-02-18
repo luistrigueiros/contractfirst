449d56e26dd408b08401bcc154811f79cd5ab4f7
luist <luistrigueiros@gmail.com>
2024-02-18 01:25:54 +0000

    incremental save, tidy up

--- src/main/java/com/example/contractfirst/domain/StudentRepository.java
+++ src/main/java/com/example/contractfirst/domain/StudentRepository.java
@@ -2,7 +2,5 @@ package com.example.contractfirst.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface StudentRepository extends CrudRepository<StudentRecord, Long>  {
 }
