275da15aa2e1313dae77bda9dc8a0c6d10368f62
luist <luistrigueiros@gmail.com>
2024-02-18 00:55:11 +0000

    Loading seed data from CSV file

--- src/main/java/com/example/contractfirst/SeedDataInitializer.java
+++ src/main/java/com/example/contractfirst/SeedDataInitializer.java
@@ -1,54 +1,46 @@
 package com.example.contractfirst;
 
+import com.example.contractfirst.domain.CourseRecord;
 import com.example.contractfirst.domain.CourseRepository;
-import com.example.contractfirst.domain.StudentRepository;
-import com.example.openapi.models.Course;
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.io.FileReader;
+import java.io.Reader;
 
 @Slf4j
 @Component
 public class SeedDataInitializer {
-    private final CourseRepository courseRepository;
-
-    private final StudentRepository studentRepository;
-
-    private final AtomicInteger courseCounter = new AtomicInteger(1);
-
-    private static List<Course> courseList = new ImmutableList
-            .Builder<Course>()
-            .add(new Course("Computer Science", "4", Course.CourseTypeEnum.ENGINEERING))
-            .add(new Course("Mechanical Engineering", "4", Course.CourseTypeEnum.ENGINEERING))
-            .add(new Course("Microbiology", "4", Course.CourseTypeEnum.MEDICAL))
-            .build();
-
-    public SeedDataInitializer(CourseRepository courseRepository, StudentRepository studentRepository) {
-        this.courseRepository = courseRepository;
-        this.studentRepository = studentRepository;
-    }
-
-    private Course setIdIn(Course course) {
-        BigDecimal courseId = new BigDecimal(courseCounter.incrementAndGet());
-        course.courseId(courseId);
-        return course;
-    }
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    @Value("classpath:/COURSE_RECORD.csv")
+    private Resource courseCsv;
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadTestData() throws Exception {
         log.info("Creating seed data ....");
-        long count = courseList.stream()
-                .map(this::setIdIn)
-                .map(Mappers::from)
-                .peek(courseRepository::save)
-                .toList()
-                .size();
+        int count = 0;
+        try (Reader reader = new FileReader(courseCsv.getFile());
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
+            for (CSVRecord record : csvParser) {
+                CourseRecord courseRecord = new CourseRecord();
+                courseRecord.setCourseId(Integer.valueOf(record.get("COURSE_ID")));
+                courseRecord.setCourseDuration(Integer.valueOf(record.get("COURSE_DURATION")));
+                courseRecord.setCourseName(record.get("COURSE_NAME"));
+                courseRecord.setCourseType(record.get("COURSE_TYPE"));
+                courseRepository.save(courseRecord);
+                count++;
+            }
+        }
         log.info("Loaded {} courses", count);
     }
 }
