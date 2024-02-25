fae11de024317a4c858b0c2b5e5902a4c1ea3207
luist <luistrigueiros@gmail.com>
2024-02-25 13:29:09 +0000

    Now able to access the user principal in one authenticated request

--- src/main/java/com/example/contractfirst/web/CoursesControllerApiImpl.java
+++ src/main/java/com/example/contractfirst/web/CoursesControllerApiImpl.java
@@ -1,29 +1,49 @@
 package com.example.contractfirst.web;
 
-import com.example.contractfirst.util.Mappers;
 import com.example.contractfirst.domain.CourseRecord;
 import com.example.contractfirst.domain.CourseRepository;
+import com.example.contractfirst.util.Mappers;
 import com.example.openapi.CoursesApi;
 import com.example.openapi.models.Course;
 import com.google.common.collect.ImmutableList;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "${app.apiRoot}")
 public class CoursesControllerApiImpl implements CoursesApi {
     private final CourseRepository courseRepository;
+    private final NativeWebRequest request;
 
-    public CoursesControllerApiImpl(CourseRepository courseRepository) {
+    public CoursesControllerApiImpl(CourseRepository courseRepository, NativeWebRequest request) {
         this.courseRepository = courseRepository;
+        this.request = request;
+    }
+
+    private void debugPrintUserPrincipal() {
+        Authentication authentication = (Authentication) request.getUserPrincipal();
+        if (authentication != null) {
+            log.debug("Is authenticate ={}", authentication.isAuthenticated());
+            List<String> list = authentication.getAuthorities()
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .toList();
+            log.debug("Granted authorities {}" , String.join(",", list));
+        }
     }
 
     @Override
     public ResponseEntity<List<Course>> getCourses(String sortBy) {
+        debugPrintUserPrincipal();
         Iterable<CourseRecord> all = courseRepository.findAll();
         List<Course> list = ImmutableList.copyOf(all)
                 .stream()
